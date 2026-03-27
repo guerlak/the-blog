20e38d1ef3683c2ac8dd63b73ecfffe1e6fa5018
Guerlak <guerlak@gmail.com>
2026-03-26 23:18:28 -0300

    feat: deleteBtn confirmation

--- src/app/admin/AdminDeleteBtn.tsx
+++ src/app/admin/AdminDeleteBtn.tsx
@@ -1,7 +1,8 @@
 'use client'
-import { Trash2 } from "lucide-react";
-import { useTransition } from "react";
+import { Trash2, Check, X } from "lucide-react";
+import { useState, useTransition } from "react";
 import { deletePostAction } from "./posts/actions";
+
 type AdminDeleteBtnProps = {
     id: string;
     title: string;
@@ -10,6 +11,11 @@ type AdminDeleteBtnProps = {
 export default function AdminDeleteBtn({ id, title }: AdminDeleteBtnProps) {
 
     const [isPending, startTransition] = useTransition();
+    const [isConfirming, setIsConfirming] = useState(false);
+
+    const handleConfirm = () => {
+        setIsConfirming(!isConfirming);
+    }
 
     const handleDelete = async (id: string) => {
         startTransition(async () => {
@@ -26,13 +32,18 @@ export default function AdminDeleteBtn({ id, title }: AdminDeleteBtnProps) {
             hover:border-rose-100 cursor-pointer
             disabled:opacity-50 disabled:cursor-not-allowed
             disabled:hover:bg-transparent
+            relative
             "
             title="Delete Post"
             aria-label={`Delete post ${title}`}
-            onClick={() => handleDelete(id)}
+            onClick={handleConfirm}
             disabled={isPending}
         >
             <Trash2 className="w-5 h-5" />
+            {isConfirming && <div className="">
+                <button onClick={() => handleDelete(id)} className="flex items-center gap-2 absolute -bottom-6 -left-2 cursor-pointer"><Check color="green" /></button>
+                <button onClick={() => handleConfirm} className="flex items-center gap-2 absolute -bottom-6 -right-2 cursor-pointer"><X color="red" /></button>
+            </div>}
         </button>
     )
 }
